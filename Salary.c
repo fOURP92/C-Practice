6926e6580ad6117c5d072a45db10e4fad808ec83
fOURP92 <panos9229@gmail.com>
2017-12-20 02:10:24 +0200

    Added Salary

---
+++ Salary.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+   {
+    double  imeromisthio, Salary;
+    int ID, hours, wromisthio;
+    printf(" Input your ID: ");
+   
+    scanf(" %d" , &ID );
+    
+    printf(" Input working hours: ");
+    
+    scanf("%d" , &hours ) ;
+   
+    printf(" Input your wromisthio: ");
+   
+    scanf("%d", &wromisthio);
+    
+    imeromisthio = wromisthio * hours;
+    
+    Salary = imeromisthio * 30;
+    
+    printf(" Ton mina pairneis: %0.2lf" , Salary);
+    
+    return 0;
+}
